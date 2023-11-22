8492ec5ba9ac8b1d08c4208e7bc87b4aba7cc89d
MiltonRamos2002 <milton.ramos954@ist17dejulio.edu.ec>
2023-11-22 10:55:37 -0500

    Operacion de Fibonacci

---
+++ app/src/main/java/com/ggvc/calculosmatematicosapp/FibonacciActivity.java
@@ -0,0 +1,51 @@
+package com.ggvc.calculosmatematicosapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class FibonacciActivity extends AppCompatActivity {
+    EditText editText;
+    Button button;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fibonacci);
+
+        editText = (EditText) findViewById(R.id.editTextNumberSigned);
+        button = (Button) findViewById(R.id.button);
+        textView =(TextView)  findViewById(R.id.TexView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(fibonacci(Integer.parseInt(editText.getText().toString())));
+            }
+        });
+
+    }
+    private String fibonacci(int n){
+        String text = "";
+        ArrayList<BigInteger> fib = new ArrayList<>();
+        BigInteger first = new BigInteger("0");
+        BigInteger second = new BigInteger("1");
+        fib.add(first);
+        fib.add(second);
+        for (int i = 1; i < n; i++){
+            fib.add(fib.get(1).add(fib.get(i - 1)));
+        }
+        for (int i = 1; i<= n; i++){
+            text = text + i + ". " + fib.get(i) + "\n";
+        }
+        return  text;
+    }
+}
