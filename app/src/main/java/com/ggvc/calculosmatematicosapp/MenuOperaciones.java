ce9ca7b6b5a7af7dc0360a8fa5760ba2dbc66fe7
RyCry7 <richely.almeida825@ist17dejulio.edu.ec>
2023-11-27 01:02:03 -0500

    calcular numeros perfectos

--- app/src/main/java/com/ggvc/calculosmatematicosapp/MenuOperaciones.java
+++ app/src/main/java/com/ggvc/calculosmatematicosapp/MenuOperaciones.java
@@ -1,12 +1,17 @@
 package com.ggvc.calculosmatematicosapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 public class MenuOperaciones extends AppCompatActivity {
 
@@ -17,21 +22,26 @@ public class MenuOperaciones extends AppCompatActivity {
 
         // Obtener referencias a los elementos de la interfaz
         TextView titleTextView = findViewById(R.id.titleTextView);
-        GridLayout gridLayout = findViewById(R.id.gridLayout);
+        ImageView gifBackground = findViewById(R.id.gifBackground);
+        Button button2 = findViewById(R.id.button2);
 
         // Configurar el título
         titleTextView.setText("Título de la Actividad");
 
-        // Agregar botones al GridLayout
-        for (int i = 1; i <= 12; i++) {
-            Button button = new Button(this);
-            button.setText("Botón " + i);
+        // Cargar el GIF en el ImageView usando Glide
+        Glide.with(this)
+                .asGif()
+                .load(R.drawable.lineas)
+                .into(gifBackground);
 
-            // Configurar el botón en el GridLayout
-            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-            params.setGravity(Gravity.FILL);
-            button.setLayoutParams(params);
-            gridLayout.addView(button);
-        }
+        // Configurar el OnClickListener para el Button2
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Abrir la actividad MainActivitySumar
+                Intent intent = new Intent(MenuOperaciones.this, MainActivitySumar.class);
+                startActivity(intent);
+            }
+        });
     }
 }
