8a7f3bc050765645738f28e248f01b23fbe9db0d
Henry Paul Diaz Pinchao <henry.diaz054@ist17dejulio.edu.ec>
2023-11-27 11:40:22 -0500

    correccion de errores

--- app/src/main/java/com/ggvc/calculosmatematicosapp/TrianguloActivity.java
+++ app/src/main/java/com/ggvc/calculosmatematicosapp/TrianguloActivity.java
@@ -18,7 +18,7 @@ public class TriangulosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_triangulos);
+        setContentView(R.layout.activity_triangulo);
         txtAltura = findViewById(R.id.txtAltura);
         txtBase =findViewById(R.id.txtBase);
         btnEquilatero = findViewById(R.id.btnEquilatero);
